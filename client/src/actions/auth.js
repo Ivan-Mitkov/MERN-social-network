b3a02beaf3843a4acd44115b623d24bcc06d5138
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-09-27 18:00:50 +0300

    logout user

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,6 +7,7 @@ import {
   AUTH_ERROR,
   LOGIN_FAIL,
   LOGIN_SUCCESS,
+  LOGOUT,
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 //check if user is autenticated and load user
@@ -94,4 +95,11 @@ export const login = (email, password) => async (dispatch) => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+//Logout user
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("token");
+  dispatch({ type: LOGOUT });
+  // dispatch(loadUser());
 };
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,6 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../../actions/auth";
+
 const Navbar = () => {
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const { isAuthenticated } = auth;
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+  const authLinks = (
+    <ul>
+      <li>
+        <Link onClick={handleLogout} to="#!">
+          <i className="fas fa-sign-out-alt"></i>
+          <span className="hide-sm">Logout</span>
+        </Link>
+      </li>
+    </ul>
+  );
+  const guestLinks = (
+    <ul>
+      <li>
+        <Link to="/profiles">Developers</Link>
+      </li>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </ul>
+  );
   return (
     <nav className="navbar bg-dark">
       <h1>
@@ -8,17 +41,7 @@ const Navbar = () => {
           <i className="fas fa-code"></i> DevConnector
         </Link>
       </h1>
-      <ul>
-        <li>
-          <Link to="/profiles">Developers</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
+      <>{isAuthenticated ? authLinks : guestLinks}</>
     </nav>
   );
 };
