4af1c21ff7564f2f5ce9cf554724cf65c95c9c2e
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-09-28 12:11:47 +0300

    profile action and reducer, getCurrentUser

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useSelector, shallowEqual } from "react-redux";
+import { getCurrentProfile } from "../../actions/profile";
 
 const Dashboard = () => {
-  return (
-    <div>
-      Dashboard
-    </div>
-  )
-}
+  const dispatch = useDispatch();
+  const auth = useSelector(
+    (state) => state.auth,
+    (state) => state.profile,
+    shallowEqual
+  );
+  React.useEffect(() => {
+    dispatch(getCurrentProfile());
+  }, []);
+  return <div>Dashboard</div>;
+};
 
-export default Dashboard
+export default Dashboard;
